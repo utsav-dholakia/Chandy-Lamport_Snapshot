4e37810dfbc8027fc3b4c5681e7c79f58a3034f9
Avinash Mishra <avinashmishra001@gmail.com>
2017-03-12 16:13:51 -0500

    map protocol addition

---
+++ src/dev/MapProtocol.java
@@ -0,0 +1,17 @@
+package dev;
+
+public class MapProtocol extends Thread{
+
+	MapProtocol(){
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(App.mapProtocolTerminationFlag == false){
+			
+		}
+		
+	}
+
+}
